a36952afea49055846c58d8bc530a707eaccf184
Innnsane <84345955+Innnsane@users.noreply.github.com>
2026-08-31 14:47:10 +0800

    feat: node base

--- core/node.go
+++ core/node.go
@@ -1,5 +1,13 @@
 package core
 
 type Node struct {
-	NodeId string
+	id        string
+	actors    map[string]chan<- Message
+	inflight  map[int16]Inflight
+	snapshots map[string]Snapshot
+	shards    []int16
+}
+
+func NewNode(id string) *Node {
+	return &Node{}
 }
---
+++ core/shard.go
@@ -0,0 +1,7 @@
+package core
+
+const Shard int16 = 1024
+
+func ActorShard(actorId string) int16 {
+	return 0
+}
