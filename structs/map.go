32ea41cc58b4eed29352b89ad0b88ff254d2ee2f
Innnsane <84345955+Innnsane@users.noreply.github.com>
2026-09-09 18:09:54 +0800

    fix: delete map increase

--- structs/map.go
+++ structs/map.go
@@ -1,18 +1,5 @@
 package structs
 
-func MapIncrease[K comparable, V int | int8 | int16 | int32 | int64](m map[K]V, key K, add V) (result V) {
-	if m == nil {
-		m = make(map[K]V)
-	}
-	_, ok := m[key]
-	if !ok {
-		m[key] = add
-	} else {
-		m[key] += add
-	}
-	return m[key]
-}
-
 type Map[K comparable, V any] struct {
 	m map[K]V
 }
